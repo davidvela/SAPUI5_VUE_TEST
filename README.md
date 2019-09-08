b528c6b589db58c7bce7240eda9a67f81c25b976
davidvela <david.vela.tirado@gmail.com>
2019-09-08 21:40:16 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# SAP/fundamental-vue test
+
+## Links
+* https://github.com/SAP/fundamental-vue/tree/master/ci-scripts
+* 
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"test/vueFundamental/test/unit/controller/Main.controller"
+], function () {
+	"use strict";
+});
